bb465d025388b0ddfad730997eaa3abd920f3196
Cong Bom <advcong@gmail.com>
2014-08-26 23:36:25 +0700

    upload

---
+++ templates/page.tpl.php
@@ -0,0 +1,11 @@
+<div id="header">
+	<?php print render($page['header']); ?>
+</div>
+
+<div id="content">	
+	<?php print render($page['content']);?>	
+</div>
+
+<div id="footer">
+	<?php print render($page['footer']);?>	
+</div>
